8860fbbca6bd4406443752d7d61ec7801e647aed
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-10-01 12:01:00 -0600

    Apply prototype to ormlq

--- projects/rocsolver/library/src/auxiliary/rocauxiliary_ormlq_unmlq.cpp
+++ projects/rocsolver/library/src/auxiliary/rocauxiliary_ormlq_unmlq.cpp
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * Copyright (C) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -56,8 +56,8 @@ rocblas_status rocsolver_ormlq_unmlq_impl(rocblas_handle handle,
         return st;
 
     // working with unshifted arrays
-    rocblas_int shiftA = 0;
-    rocblas_int shiftC = 0;
+    rocblas_stride shiftA = 0;
+    rocblas_stride shiftC = 0;
 
     // normal (non-batched non-strided) execution
     rocblas_stride strideA = 0;
@@ -66,41 +66,23 @@ rocblas_status rocsolver_ormlq_unmlq_impl(rocblas_handle handle,
     rocblas_int batch_count = 1;
 
     // memory workspace sizes:
-    // size for constants in rocblas calls
-    size_t size_scalars;
-    // extra requirements for calling ORM2R/UNM2R or LARFT + LARFB
-    size_t size_AbyxORwork, size_diagORtmptr;
-    // size of temporary array for triangular factor
-    size_t size_trfact;
-    // size of arrays of pointers (for batched cases)
-    size_t size_workArr;
-    rocsolver_ormlq_unmlq_getMemorySize<false, T>(side, m, n, k, batch_count, &size_scalars,
-                                                  &size_AbyxORwork, &size_diagORtmptr, &size_trfact,
-                                                  &size_workArr);
+    rocsolver_workspace_helper work_helper;
+    rocsolver_ormlq_unmlq_getMemorySize<false, T>(side, trans, m, n, k, batch_count, &work_helper);
 
     if(rocblas_is_device_memory_size_query(handle))
-        return rocblas_set_optimal_device_memory_size(handle, size_scalars, size_AbyxORwork,
-                                                      size_diagORtmptr, size_trfact, size_workArr);
+        return rocblas_set_optimal_device_memory_size(handle, work_helper.get_total_size<T>());
 
     // memory workspace allocation
-    void *scalars, *AbyxORwork, *diagORtmptr, *trfact, *workArr;
-    rocblas_device_malloc mem(handle, size_scalars, size_AbyxORwork, size_diagORtmptr, size_trfact,
-                              size_workArr);
+    rocblas_device_malloc mem(handle, work_helper.get_total_size<T>());
     if(!mem)
         return rocblas_status_memory_error;
 
-    scalars = mem[0];
-    AbyxORwork = mem[1];
-    diagORtmptr = mem[2];
-    trfact = mem[3];
-    workArr = mem[4];
-    if(size_scalars > 0)
-        init_scalars(handle, (T*)scalars);
+    ROCBLAS_CHECK(work_helper.assign_buffer<T>(handle, mem[0]));
 
     // execution
-    return rocsolver_ormlq_unmlq_template<false, false, T>(
-        handle, side, trans, m, n, k, A, shiftA, lda, strideA, ipiv, strideP, C, shiftC, ldc,
-        strideC, batch_count, (T*)scalars, (T*)AbyxORwork, (T*)diagORtmptr, (T*)trfact, (T**)workArr);
+    return rocsolver_ormlq_unmlq_template<false, T>(handle, side, trans, m, n, k, A, shiftA, lda,
+                                                    strideA, ipiv, strideP, C, shiftC, ldc, strideC,
+                                                    batch_count, &work_helper);
 }
 
 ROCSOLVER_END_NAMESPACE
--- projects/rocsolver/library/src/auxiliary/rocauxiliary_ormlq_unmlq.hpp
+++ projects/rocsolver/library/src/auxiliary/rocauxiliary_ormlq_unmlq.hpp
@@ -4,7 +4,7 @@
  *     Univ. of Tennessee, Univ. of California Berkeley,
  *     Univ. of Colorado Denver and NAG Ltd..
  *     December 2016
- * Copyright (C) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,6 +37,7 @@
 #include "rocauxiliary_orml2_unml2.hpp"
 #include "rocblas.hpp"
 #include "rocsolver/rocsolver.h"
+#include "rocsolver_workspace_helper.hpp"
 
 ROCSOLVER_BEGIN_NAMESPACE
 
@@ -84,6 +85,50 @@ void rocsolver_ormlq_unmlq_getMemorySize(const rocblas_side side,
     }
     else
         *size_trfact = 0;
+}
+
+template <bool BATCHED, typename T, bool COMPLEX = rocblas_is_complex<T>>
+void rocsolver_ormlq_unmlq_getMemorySize(const rocblas_side side,
+                                         const rocblas_operation trans,
+                                         const rocblas_int m,
+                                         const rocblas_int n,
+                                         const rocblas_int k,
+                                         const rocblas_int batch_count,
+                                         rocsolver_workspace_helper* work_helper)
+{
+    // if quick return no workspace needed
+    if(m == 0 || n == 0 || k == 0 || batch_count == 0)
+        return;
+
+    if(k <= xxMxQ_BLOCKSIZE)
+    {
+        rocsolver_orml2_unml2_getMemorySize<BATCHED, T>(side, m, n, k, batch_count, work_helper);
+        return;
+    }
+    else
+    {
+        work_helper->set_nested_capacity(2);
+
+        rocblas_int jb = xxMxQ_BLOCKSIZE;
+        rocblas_operation transB;
+        if(trans != rocblas_operation_none)
+            transB = rocblas_operation_none;
+        else
+            transB = (COMPLEX ? rocblas_operation_conjugate_transpose : rocblas_operation_transpose);
+
+        // requirements for calling larft
+        rocsolver_larft_getMemorySize<BATCHED, T>(std::max(m, n), std::min(jb, k), batch_count,
+                                                  work_helper->add_nested());
+
+        // requirements for calling larfb
+        rocsolver_larfb_getMemorySize<BATCHED, T>(side, transB, m, n, std::min(jb, k), batch_count,
+                                                  work_helper->add_nested());
+
+        // size of temporary array for triangular factor
+        size_t size_trfact = sizeof(T) * jb * jb * batch_count;
+
+        work_helper->assign_sizes({size_trfact});
+    }
 }
 
 template <bool BATCHED, bool STRIDED, typename T, typename U, bool COMPLEX = rocblas_is_complex<T>>
@@ -200,6 +245,125 @@ rocblas_status rocsolver_ormlq_unmlq_template(rocblas_handle handle,
             handle, side, transB, rocblas_forward_direction, rocblas_row_wise, nrow, ncol, ib, A,
             shiftA + idx2D(i, i, lda), lda, strideA, trfact, 0, ldw, strideW, C,
             shiftC + idx2D(ic, jc, ldc), ldc, strideC, batch_count, diagORtmptr, workArr);
+    }
+
+    return rocblas_status_success;
+}
+
+template <bool BATCHED, typename T, typename U, bool COMPLEX = rocblas_is_complex<T>>
+rocblas_status rocsolver_ormlq_unmlq_template(rocblas_handle handle,
+                                              const rocblas_side side,
+                                              const rocblas_operation trans,
+                                              const rocblas_int m,
+                                              const rocblas_int n,
+                                              const rocblas_int k,
+                                              U A,
+                                              const rocblas_stride shiftA,
+                                              const rocblas_int lda,
+                                              const rocblas_stride strideA,
+                                              T* ipiv,
+                                              const rocblas_stride strideP,
+                                              U C,
+                                              const rocblas_stride shiftC,
+                                              const rocblas_int ldc,
+                                              const rocblas_stride strideC,
+                                              const rocblas_int batch_count,
+                                              rocsolver_workspace_helper* work_helper)
+{
+    ROCSOLVER_ENTER("ormlq_unmlq", "side:", side, "trans:", trans, "m:", m, "n:", n, "k:", k,
+                    "shiftA:", shiftA, "lda:", lda, "shiftC:", shiftC, "ldc:", ldc,
+                    "bc:", batch_count);
+
+    // quick return
+    if(!n || !m || !k || !batch_count)
+        return rocblas_status_success;
+
+    hipStream_t stream;
+    rocblas_get_stream(handle, &stream);
+
+    // if the matrix is small, use the unblocked variant of the algorithm
+    if(k <= xxMxQ_BLOCKSIZE)
+        return rocsolver_orml2_unml2_template<T>(handle, side, trans, m, n, k, A, shiftA, lda,
+                                                 strideA, ipiv, strideP, C, shiftC, ldc, strideC,
+                                                 batch_count, work_helper);
+
+    // prepare workspace
+    auto larft_work = work_helper->get_nested(0);
+    auto larfb_work = work_helper->get_nested(1);
+    T* trfact = (T*)(*work_helper)[0];
+
+    rocblas_int ldw = xxMxQ_BLOCKSIZE;
+    rocblas_stride strideW = rocblas_stride(ldw) * ldw;
+
+    // determine limits and indices
+    bool left = (side == rocblas_side_left);
+    bool transpose = (trans != rocblas_operation_none);
+    rocblas_int start, step, nq, ncol, nrow, ic, jc;
+    if(left)
+    {
+        nq = m;
+        ncol = n;
+        jc = 0;
+        if(!transpose)
+        {
+            start = 0;
+            step = 1;
+        }
+        else
+        {
+            start = (k - 1) / ldw * ldw;
+            step = -1;
+        }
+    }
+    else
+    {
+        nq = n;
+        nrow = m;
+        ic = 0;
+        if(!transpose)
+        {
+            start = (k - 1) / ldw * ldw;
+            step = -1;
+        }
+        else
+        {
+            start = 0;
+            step = 1;
+        }
+    }
+
+    rocblas_operation transB;
+    if(transpose)
+        transB = rocblas_operation_none;
+    else
+        transB = (COMPLEX ? rocblas_operation_conjugate_transpose : rocblas_operation_transpose);
+
+    rocblas_int i, ib;
+    for(rocblas_int j = 0; j < k; j += ldw)
+    {
+        i = start + step * j; // current householder block
+        ib = std::min(ldw, k - i);
+        if(left)
+        {
+            nrow = m - i;
+            ic = i;
+        }
+        else
+        {
+            ncol = n - i;
+            jc = i;
+        }
+
+        // generate triangular factor of current block reflector
+        rocsolver_larft_template<T>(handle, rocblas_forward_direction, rocblas_row_wise, nq - i, ib,
+                                    A, shiftA + idx2D(i, i, lda), lda, strideA, ipiv + i, strideP,
+                                    trfact, ldw, strideW, batch_count, larft_work);
+
+        // apply current block reflector
+        rocsolver_larfb_template<BATCHED, T>(
+            handle, side, transB, rocblas_forward_direction, rocblas_row_wise, nrow, ncol, ib, A,
+            shiftA + idx2D(i, i, lda), lda, strideA, trfact, 0, ldw, strideW, C,
+            shiftC + idx2D(ic, jc, ldc), ldc, strideC, batch_count, larfb_work);
     }
 
     return rocblas_status_success;
