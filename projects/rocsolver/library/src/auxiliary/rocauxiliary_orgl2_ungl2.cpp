7db1cdf27c6349ed55754694041c44a50b4ceb95
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-10-01 10:51:25 -0600

    Apply prototype to orglq

--- projects/rocsolver/library/src/auxiliary/rocauxiliary_orgl2_ungl2.cpp
+++ projects/rocsolver/library/src/auxiliary/rocauxiliary_orgl2_ungl2.cpp
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * Copyright (C) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -50,7 +50,7 @@ rocblas_status rocsolver_orgl2_ungl2_impl(rocblas_handle handle,
         return st;
 
     // working with unshifted arrays
-    rocblas_int shiftA = 0;
+    rocblas_stride shiftA = 0;
 
     // normal (non-batched non-strided) execution
     rocblas_stride strideA = 0;
@@ -58,33 +58,22 @@ rocblas_status rocsolver_orgl2_ungl2_impl(rocblas_handle handle,
     rocblas_int batch_count = 1;
 
     // memory workspace sizes:
-    // size for constants in rocblas calls
-    size_t size_scalars;
-    // size of arrays of pointers (for batched cases)
-    size_t size_workArr;
-    // extra requirements for calling LARF
-    size_t size_Abyx;
-    rocsolver_orgl2_ungl2_getMemorySize<false, T>(m, n, batch_count, &size_scalars, &size_Abyx,
-                                                  &size_workArr);
+    rocsolver_workspace_helper work_helper;
+    rocsolver_orgl2_ungl2_getMemorySize<false, T>(m, n, batch_count, &work_helper);
 
     if(rocblas_is_device_memory_size_query(handle))
-        return rocblas_set_optimal_device_memory_size(handle, size_scalars, size_Abyx, size_workArr);
+        return rocblas_set_optimal_device_memory_size(handle, work_helper.get_total_size<T>());
 
     // memory workspace allocation
-    void *scalars, *Abyx, *workArr;
-    rocblas_device_malloc mem(handle, size_scalars, size_Abyx, size_workArr);
+    rocblas_device_malloc mem(handle, work_helper.get_total_size<T>());
     if(!mem)
         return rocblas_status_memory_error;
 
-    scalars = mem[0];
-    Abyx = mem[1];
-    workArr = mem[2];
-    if(size_scalars > 0)
-        init_scalars(handle, (T*)scalars);
+    ROCBLAS_CHECK(work_helper.assign_buffer<T>(handle, mem[0]));
 
     // execution
-    return rocsolver_orgl2_ungl2_template<T>(handle, m, n, k, A, shiftA, lda, strideA, ipiv, strideP,
-                                             batch_count, (T*)scalars, (T*)Abyx, (T**)workArr);
+    return rocsolver_orgl2_ungl2_template<T>(handle, m, n, k, A, shiftA, lda, strideA, ipiv,
+                                             strideP, batch_count, &work_helper);
 }
 
 ROCSOLVER_END_NAMESPACE
--- projects/rocsolver/library/src/auxiliary/rocauxiliary_orglq_unglq.hpp
+++ projects/rocsolver/library/src/auxiliary/rocauxiliary_orglq_unglq.hpp
@@ -4,7 +4,7 @@
  *     Univ. of Tennessee, Univ. of California Berkeley,
  *     Univ. of Colorado Denver and NAG Ltd..
  *     December 2016
- * Copyright (C) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,6 +37,7 @@
 #include "rocauxiliary_orgl2_ungl2.hpp"
 #include "rocblas.hpp"
 #include "rocsolver/rocsolver.h"
+#include "rocsolver_workspace_helper.hpp"
 
 ROCSOLVER_BEGIN_NAMESPACE
 
@@ -88,6 +89,46 @@ void rocsolver_orglq_unglq_getMemorySize(const rocblas_int m,
 
         // size of temporary array for triangular factor
         *size_trfact = sizeof(T) * jb * jb * batch_count;
+    }
+}
+
+template <bool BATCHED, typename T>
+void rocsolver_orglq_unglq_getMemorySize(const rocblas_int m,
+                                         const rocblas_int n,
+                                         const rocblas_int k,
+                                         const rocblas_int batch_count,
+                                         rocsolver_workspace_helper* work_helper)
+{
+    // if quick return no workspace needed
+    if(m == 0 || n == 0 || batch_count == 0)
+        return;
+
+    if(k <= xxGxQ_xxGxQ2_SWITCHSIZE)
+    {
+        rocsolver_orgl2_ungl2_getMemorySize<BATCHED, T>(m, n, batch_count, work_helper);
+        return;
+    }
+
+    else
+    {
+        work_helper->set_nested_capacity(3);
+
+        rocblas_int jb = xxGxQ_BLOCKSIZE;
+        rocblas_int j = ((k - xxGxQ_xxGxQ2_SWITCHSIZE - 1) / jb) * jb;
+        rocblas_int kk = std::min(k, j + jb);
+
+        rocsolver_orgl2_ungl2_getMemorySize<BATCHED, T>(m, n, batch_count, work_helper->add_nested());
+
+        rocsolver_larft_getMemorySize<BATCHED, T>(n, jb, batch_count, work_helper->add_nested());
+
+        rocsolver_larfb_getMemorySize<BATCHED, T>(rocblas_side_right,
+                                                  rocblas_operation_conjugate_transpose, m - jb, n,
+                                                  jb, batch_count, work_helper->add_nested());
+
+        // size of temporary array for triangular factor
+        size_t size_trfact = sizeof(T) * jb * jb * batch_count;
+
+        work_helper->assign_sizes({size_trfact});
     }
 }
 
@@ -181,6 +222,103 @@ rocblas_status rocsolver_orglq_unglq_template(rocblas_handle handle,
         rocsolver_orgl2_ungl2_template<T>(handle, jb, n - j, jb, A, shiftA + idx2D(j, j, lda), lda,
                                           strideA, (ipiv + j), strideP, batch_count, scalars,
                                           Abyx_tmptr, workArr);
+
+        j -= jb;
+    }
+
+    return rocblas_status_success;
+}
+
+template <bool BATCHED, typename T, typename U>
+rocblas_status rocsolver_orglq_unglq_template(rocblas_handle handle,
+                                              const rocblas_int m,
+                                              const rocblas_int n,
+                                              const rocblas_int k,
+                                              U A,
+                                              const rocblas_stride shiftA,
+                                              const rocblas_int lda,
+                                              const rocblas_stride strideA,
+                                              T* ipiv,
+                                              const rocblas_stride strideP,
+                                              const rocblas_int batch_count,
+                                              rocsolver_workspace_helper* work_helper)
+{
+    ROCSOLVER_ENTER("orglq_unglq", "m:", m, "n:", n, "k:", k, "shiftA:", shiftA, "lda:", lda,
+                    "bc:", batch_count);
+
+    // quick return
+    if(!n || !m || !batch_count)
+        return rocblas_status_success;
+
+    hipStream_t stream;
+    rocblas_get_stream(handle, &stream);
+
+    // if the matrix is small, use the unblocked variant of the algorithm
+    if(k <= xxGxQ_xxGxQ2_SWITCHSIZE)
+        return rocsolver_orgl2_ungl2_template<T>(handle, m, n, k, A, shiftA, lda, strideA, ipiv,
+                                                 strideP, batch_count, work_helper);
+
+    // prepare workspace
+    auto orgl2_work = work_helper->get_nested(0);
+    auto larft_work = work_helper->get_nested(1);
+    auto larfb_work = work_helper->get_nested(2);
+    T* trfact = (T*)(*work_helper)[0];
+
+    rocblas_int ldw = xxGxQ_BLOCKSIZE;
+    rocblas_stride strideW = rocblas_stride(ldw) * ldw;
+
+    // start of first blocked block
+    rocblas_int jb = ldw;
+    rocblas_int j = ((k - xxGxQ_xxGxQ2_SWITCHSIZE - 1) / jb) * jb;
+
+    // start of the unblocked block
+    rocblas_int kk = std::min(k, j + jb);
+
+    rocblas_int blocksy, blocksx;
+
+    // compute the unblockled part and set to zero the
+    // corresponding left submatrix
+    if(kk < m)
+    {
+        blocksx = (m - kk - 1) / 32 + 1;
+        blocksy = (kk - 1) / 32 + 1;
+        ROCSOLVER_LAUNCH_KERNEL(set_zero<T>, dim3(blocksx, blocksy, batch_count), dim3(32, 32), 0,
+                                stream, m - kk, kk, A, shiftA + idx2D(kk, 0, lda), lda, strideA);
+
+        rocsolver_orgl2_ungl2_template<T>(handle, m - kk, n - kk, k - kk, A,
+                                          shiftA + idx2D(kk, kk, lda), lda, strideA, (ipiv + kk),
+                                          strideP, batch_count, orgl2_work);
+    }
+
+    // compute the blocked part
+    while(j >= 0)
+    {
+        // first update the already computed part
+        // applying the current block reflector using larft + larfb
+        if(j + jb < m)
+        {
+            rocsolver_larft_template<T>(handle, rocblas_forward_direction, rocblas_row_wise, n - j,
+                                        jb, A, shiftA + idx2D(j, j, lda), lda, strideA, (ipiv + j),
+                                        strideP, trfact, ldw, strideW, batch_count, larft_work);
+
+            rocsolver_larfb_template<BATCHED, T>(
+                handle, rocblas_side_right, rocblas_operation_conjugate_transpose,
+                rocblas_forward_direction, rocblas_row_wise, m - j - jb, n - j, jb, A,
+                shiftA + idx2D(j, j, lda), lda, strideA, trfact, 0, ldw, strideW, A,
+                shiftA + idx2D(j + jb, j, lda), lda, strideA, batch_count, larfb_work);
+        }
+
+        // now compute the current block and set to zero
+        // the corresponding top submatrix
+        if(j > 0)
+        {
+            blocksx = (jb - 1) / 32 + 1;
+            blocksy = (j - 1) / 32 + 1;
+            ROCSOLVER_LAUNCH_KERNEL(set_zero<T>, dim3(blocksx, blocksy, batch_count), dim3(32, 32),
+                                    0, stream, jb, j, A, shiftA + idx2D(j, 0, lda), lda, strideA);
+        }
+        rocsolver_orgl2_ungl2_template<T>(handle, jb, n - j, jb, A, shiftA + idx2D(j, j, lda), lda,
+                                          strideA, (ipiv + j), strideP, batch_count, orgl2_work);
 
         j -= jb;
     }
